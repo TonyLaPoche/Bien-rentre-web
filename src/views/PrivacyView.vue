8f6adb1b42791f4fb6e46cba0dc26617721a5e30
TonyLaPoche <contact@bienrentre.app>
2025-11-06 23:04:22 +0100

    debug: add temporary debug to PrivacyView to test component loading

--- src/views/PrivacyView.vue
+++ src/views/PrivacyView.vue
@@ -3,6 +3,11 @@
     <div class="container">
       <header class="page-header">
         <div class="header-content">
+          <!-- DEBUG: Test if component loads -->
+          <div style="background: red; color: white; padding: 10px; margin-bottom: 20px;">
+            DEBUG: PrivacyView component loaded! Current route: {{ $route.path }}
+          </div>
+
           <router-link to="/" class="back-link">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
